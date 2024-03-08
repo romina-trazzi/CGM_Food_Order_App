a8930663da123ff9fe8193c7982b03bbcdcca89d
Romina Trazzi <romina.trazzi@posta.it>
2024-03-09 00:46:48 +0100

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import {fetchMeals} from './http.js'
 import styled from 'styled-components';
+import Header from './components/Header.jsx'
 
 export const Main = styled.main `
   display: flex;
@@ -11,33 +12,61 @@ export const MealContainer = styled.div`
   width: 70%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: auto;
+  grid-template-rows: repeat(7, 1fr);
   gap: 1rem;
 `
 
-export const SingleMeal = styled.div`
+export const MealCard = styled.div`
  display: flex;
  flex-direction: column;
- height: min-content;
+ height: max-content;
  border: 1px solid black;
+ border-radius: 30px;
+ background-color: rgba(30, 30, 30, 0.8);
 `
 
 export const MealImg = styled.img`
   width: 100%;
-  height: 33%;
+  border-radius: 30px;
 `
 
 export const MealData = styled.div`
   width: 100%;
+  height: 20rem;
+  text-align: center;
+  padding: 1rem;
+
+  & div {
+    display: flex;
+    justify-content: center;
+
+    & p {
+      padding: 0.6rem 2rem;
+      color: gold; 
+      background-color: rgba(51, 51, 51, 0.8);
+      border-radius: 0.5rem;
+    }
+  }
+
 `
 
 export const MealButton = styled.button`
-  background-color: yellow;
-  width: 20%;
-  border-radius: 10px;
-  padding: 0.5rem 1rem;
+  background-color: gold;
+  width: 30%;
+  border-radius: 5px;
+  padding: 0.5rem;
+  margin-top: 2rem;
 `
+export const Footer = styled.footer `
+ background-color: black;
+ color: white;
+ text-align: center;
+ vertical-align: middle;
+ height: 3rem;
+ margin-top: 5rem;
+ line-height: 3rem;
 
+`
 
 function App() {
 
@@ -61,28 +90,30 @@ function App() {
 
   return (
     <>
-      <header>Pizza</header>
+      <Header/>
       <Main>
         
         <MealContainer>
           
           {meals.map(meal => (
-            <SingleMeal key={meal.id}>
+            <MealCard key={meal.id}>
               <MealImg src={`/backend/public/${meal.image}`}></MealImg>
               <MealData>
-                <p>{meal.name}</p>
-                <p>{meal.price}</p>
-                <p>{meal.description}</p>
+                <p><b>{meal.name}</b></p>
+                <div>
+                  <p>$ {meal.price}</p>
+                </div>
+                <p style={{minHeight: '3rem'}}>{meal.description}</p>
+                <MealButton>Add to cart</MealButton>
               </MealData>
-              <MealButton>Add to cart</MealButton>
-            </SingleMeal>
+            </MealCard>
           ))} 
   
         </MealContainer>
        
 
       </Main>
-      <footer>Final Project CGM React Course. Made by ~ Romina Trazzi - 2024 ~</footer>
+      <Footer>Final Project CGM React Course. Made by ~ Romina Trazzi - 2024 ~</Footer>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const HeaderStyle = styled.header `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 10%;
+`
+
+
+
+function Header() {
+  return (
+    <div>Header Pizza</div>
+  )
+}
+
+export default Header
