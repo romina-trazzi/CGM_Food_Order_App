30ef9d97e3f38ae8f255a8e7210406521dc4136a
Romina Trazzi <romina.trazzi@posta.it>
2024-03-12 14:24:00 +0100

    Cart almost done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,21 @@
 import { useContext } from 'react';
 import { CartContext } from './store/shoppingCartContext.jsx';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const MealList = styled.ul `
   list-style: none;
   margin: 0.5rem 0;
   padding: 0;
-`
+`;
 export const MealRow = styled.li `
   padding: 0.5rem;
   padding-right: 0;
-`
+`;
 export const MealData = styled.div`
   display: grid; 
   grid-gap: 4rem;
   grid-template-columns: 1fr 1fr;
-`
+`;
 export const MealUpdateButton = styled.button`
   cursor: pointer;
   font-size: 1rem;
@@ -34,8 +34,7 @@ export const MealUpdateButton = styled.button`
     border-color: #ffab04;
     color: #1f1a09; 
   }
-  
-`
+`;
 export const MealCartTotal = styled.p`
   display: flex;
   justify-content: flex-end;
@@ -43,7 +42,7 @@ export const MealCartTotal = styled.p`
   font-size: 1.15rem;
   font-weight: bold;
   color: #46443c;
-`
+`;
 
 function Cart() {
   const { meals, updateMealQuantity } = useContext(CartContext);
@@ -53,33 +52,43 @@ function Cart() {
   );
 
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+  const handleIncrementQuantity = (event, mealId) => {
+    event.preventDefault();
+    console.log("Hello +")
+    updateMealQuantity(mealId, 1);
+  };
+
+  const handleDecrementQuantity = (event, mealId) => {
+    event.preventDefault();
+    console.log("Hello -");
+    updateMealQuantity(mealId, -1);
+  };
   
   return (
     <div>
       {meals.length === 0 && <p>No items in cart!</p>}
       {meals.length > 0 && (
         <MealList>
-          {meals.map((meal) => {
-            return (
-              <MealRow key={meal.id}>
-                <MealData>
-                  <div>{meal.name} - ({meal.quantity} x {meal.price})</div>
-                  <div style={{display: "flex", justifyContent: "end"}}>
-                    <MealUpdateButton onClick={() => updateMealQuantity(meal.id, 1)}> + </MealUpdateButton>
-                    <span style={{paddingLeft: "1rem", paddingRight: "1rem"}}>{meal.quantity}</span>
-                    <MealUpdateButton onClick={() => updateMealQuantity(meal.id, -1)}> - </MealUpdateButton>
-                  </div>
-                </MealData>
-              </MealRow>
-              );
-            }
-          )}
+          {meals.map((meal) => (
+            <MealRow key={meal.id}>
+              <MealData>
+                <div>{meal.name} - ({meal.quantity} x {meal.price})</div>
+                <div style={{display: "flex", justifyContent: "end"}}>
+                  <MealUpdateButton onClick={(e) => handleIncrementQuantity(e, meal.id)}> + </MealUpdateButton>
+                  <span style={{paddingLeft: "1rem", paddingRight: "1rem"}}>{meal.quantity}</span>
+                  <MealUpdateButton onClick={(e) => handleDecrementQuantity(e, meal.id)}> - </MealUpdateButton>
+                </div>
+              </MealData>
+            </MealRow>
+          ))}
         </MealList>
       )}
       <MealCartTotal><strong>{formattedTotalPrice}</strong></MealCartTotal>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
+
 
--- src/components/store/shoppingCartContext.jsx
+++ src/components/store/shoppingCartContext.jsx
@@ -21,59 +21,55 @@ Se non li mettiamo l'applicazione funziona lo stesso ma diventa più difficile *
 function shoppingCartReducer(state, action) {
 
     if (action.type === 'ADD_MEAL') {
-    
-        // Importa e assegna il meal selezionato (viene passato da addItemToCart che prende come argomento il meal al click del button)
-        const selectedMeal = action.payload;
-        
-        // Trasforma il meal selezionato in un "meal del carrello"
-        const existingMeal = state.meals.find(meal => meal.id === selectedMeal.id);
 
-    
-        if (existingMeal) {
-        
-            // Se il pasto esiste già nel carrello, aumenta solo la quantità di quello (creando un array nuovo con map, quindi no bisogno di prev)
-            const updatedMeals = state.meals.map(meal => meal.id === selectedMeal.id ? { ...meal, quantity: meal.quantity + 1 } : meal
-        );
+    // Importa e assegna il meal selezionato (viene passato da addItemToCart che prende come argomento il meal al click del button)
+    const selectedMeal = action.payload;
+
+    // Trasforma il meal selezionato in un "meal del carrello" cercando l'indice (se non c'è l'indice vale -1)
+    const existingMealIndex = state.meals.findIndex(meal => meal.id === selectedMeal.id);
+
+
+        // Se il pasto esiste già nel carrello, aumenta solo la quantità di quello (creando un array nuovo con map, quindi no bisogno di prev)
+        if (existingMealIndex !== -1) {
+            const updatedMeals = [...state.meals];
+            updatedMeals[existingMealIndex].quantity += 1;
             
-        
-        // Tieni aggiornato lo stato
-        return {
+            // Tieni aggiornato lo stato
+            return {
             ...state,
             meals: updatedMeals
-        };
+            };
+
 
         } else {
-        
             // Se il pasto non esiste nel carrello, aggiungilo con la quantità 1 e aggiorna lo stato
             return {
                 ...state,
                 meals: [...state.meals, { ...selectedMeal, quantity: 1 }]
             };
         }
-    }      
-
+    }
+    
     if (action.type === 'UPDATE_MEAL') {
-       
-        // Destructuring payload
-        const { productId, amount } = action.payload;
         
+        const { productId, amount } = action.payload;
+
         const updatedMeals = state.meals.map(meal =>
-            meal.id === productId ? { ...meal, quantity: amount } : meal
+        meal.id === productId ? { ...meal, quantity: meal.quantity + amount } : meal
         );
 
         return {
-            ...state,
-            meals: updatedMeals
+        ...state,
+        meals: updatedMeals
         };
+    }
 
-    } 
+    // Lo stato rimane invariato (ex-prev) per azioni non riconosciute
+    return state;
 }
-
-
 
 // 3. Leghiamo la funzione Reducer al Context tramite CartContextProvider
 export default function CartContextProvider({children}) {
-
 
     /* 3.1. 
     a) ShoppingCartState = i dati che vanno manipolati 
